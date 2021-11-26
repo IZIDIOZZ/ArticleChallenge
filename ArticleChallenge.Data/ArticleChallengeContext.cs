6957c48bf9a4bbd55dc3b416822394f6bfc34a2f
Labs <daniel.izidioparacontato@gmail.com>
2021-11-26 10:14:14 -0300

    Commit Inicial

---
+++ ArticleChallenge.Data/ArticleChallengeContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticleChallenge.Data
+{
+    class ArticleChallengeContext: DbContext
+    {
+
+        public ArticleChallengeContext(DbContextOptions<ArticleChallengeContext> options) : base(options) 
+        { 
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetAssembly(typeof(ArticleChallengeContext)));
+        }
+
+    }
+}
